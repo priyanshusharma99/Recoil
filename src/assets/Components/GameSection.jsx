9c4e7a9cfccd79755dc0a93a3c9ef3421e36410b
Priyanshu <Priyanshu@MacBook-Pro-33.local>
2025-11-15 23:21:07 +0530

    Created the game animated section

--- src/assets/Components/GameSection.jsx
+++ src/assets/Components/GameSection.jsx
@@ -4,7 +4,7 @@ const GameSection = () => {
   return (
     <div className='w-full h-[90vh] bg-black'>
       <div className="card1">
-        <img src="" alt="" />
+        <img src="GTA-6-Leak-Featured_1681695859263_1696003454576.jpg" alt="" />
       </div>
     </div>
   )
